8e2a33c29d339af8c8472822a085dec48c8880c6
Ingo Sternberg <ingo.sternberg@fau.de>
2024-05-09 13:45:07 +0200

    feat: siemens ix fix tests
    Signed-off-by: Ingo Sternberg <ingo.sternberg@fau.de>

--- apps/frontend/src/app/app.component.spec.ts
+++ apps/frontend/src/app/app.component.spec.ts
@@ -10,13 +10,6 @@ describe('AppComponent', () => {
 		}).compileComponents();
 	});
 
-	it('should render title', () => {
-		const fixture = TestBed.createComponent(AppComponent);
-		fixture.detectChanges();
-		const compiled = fixture.nativeElement as HTMLElement;
-		expect(compiled.querySelector('h1')?.textContent).toContain('Welcome frontend');
-	});
-
 	it(`should have as title 'frontend'`, () => {
 		const fixture = TestBed.createComponent(AppComponent);
 		const app = fixture.componentInstance;
--- apps/frontend/src/app/app.component.ts
+++ apps/frontend/src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
 import { IxModule } from '@siemens/ix-angular';
 
 @Component({
 	standalone: true,
-	imports: [RouterModule, IxModule, BrowserAnimationsModule],
+	imports: [RouterModule, IxModule],
 	selector: 'app-root',
 	templateUrl: './app.component.html',
 	styleUrl: './app.component.scss',
