2ce7883092ef1c6a66ddb8b6396b8fa5b5d6462f
Ingo Sternberg <ingo.sternberg@fau.de>
2024-05-10 11:50:54 +0200

    chore: get tests and build to run temporarily
    Co-authored-by: Patrick Schmidt <patrick.m.schmidt@fau.de>
    Co-authored-by: Maximillian Krug <krugm03@gmail.com>
    Signed-off-by: Ingo Sternberg <ingo.sternberg@fau.de>

--- apps/frontend/src/app/layouts/root.layout.spec.ts
+++ apps/frontend/src/app/layouts/root.layout.spec.ts
@@ -1,6 +1,25 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute } from '@angular/router';
 
+import { IBreadcrumbData } from '../components/header/header.component';
 import { AppRootLayout } from './root.layout';
+
+const HEADER_ROUTES = {
+	root: {
+		snapshot: {
+			data: {
+				breadcrumbs: { label: 'Layer 1', url: '/layer1' } as IBreadcrumbData,
+			},
+		},
+		firstChild: {
+			snapshot: {
+				data: {
+					breadcrumbs: { label: 'Layer 2', url: '/layer2' } as IBreadcrumbData,
+				},
+			},
+		},
+	},
+};
 
 describe('AppRootLayout', () => {
 	let component: AppRootLayout;
@@ -9,6 +28,12 @@ describe('AppRootLayout', () => {
 	beforeEach(async () => {
 		await TestBed.configureTestingModule({
 			imports: [AppRootLayout],
+			providers: [
+				{
+					provide: ActivatedRoute,
+					useValue: HEADER_ROUTES,
+				},
+			],
 		}).compileComponents();
 
 		fixture = TestBed.createComponent(AppRootLayout);
