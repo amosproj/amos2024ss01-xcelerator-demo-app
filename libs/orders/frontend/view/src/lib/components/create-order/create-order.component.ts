4ea5372770274300ae7c863acefd3fcddd12f100
Ingo Sternberg <ingo.sternberg@fau.de>
2024-05-17 15:45:09 +0200

    feat: start working on order form
    Co-authored-by: Patrick Schmidt <patrick.m.schmidt@fau.de>
    Signed-off-by: Ingo Sternberg <ingo.sternberg@fau.de>

--- libs/orders/frontend/view/src/lib/components/create-order/create-order.component.ts
+++ libs/orders/frontend/view/src/lib/components/create-order/create-order.component.ts
@@ -1,17 +1,25 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { IxModule } from '@siemens/ix-angular';
 
 @Component({
 	selector: 'lib-create-order',
 	standalone: true,
-	imports: [CommonModule, IxModule],
+	imports: [CommonModule, IxModule, ReactiveFormsModule],
 	templateUrl: './create-order.component.html',
 	styleUrl: './create-order.component.scss',
 	encapsulation: ViewEncapsulation.None,
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CreateOrderComponent {
+	createOrderForm = new FormGroup({
+		selectFacility: new FormControl('', Validators.required),
+		assets: new FormControl('', Validators.required),
+		phone: new FormControl('', [Validators.required, Validators.maxLength(20)]),
+		email: new FormControl('', [Validators.required, Validators.email]),
+		text: new FormControl(''),
+	});
 	onSubmit() {
 		// submit order
 	}
