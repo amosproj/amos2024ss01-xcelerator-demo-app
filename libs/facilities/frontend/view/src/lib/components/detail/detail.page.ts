3c34f42c31fddce09635fe8f95dc2fac333f998a
Ingo Sternberg <ingo.sternberg@fau.de>
2024-05-18 18:28:31 +0200

    feat: show example chart and begin to work on better routing
    Signed-off-by: Ingo Sternberg <ingo.sternberg@fau.de>

--- libs/facilities/frontend/view/src/lib/components/detail/detail.page.ts
+++ libs/facilities/frontend/view/src/lib/components/detail/detail.page.ts
@@ -1,7 +1,12 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { themeSwitcher } from '@siemens/ix';
 import { IxModule } from '@siemens/ix-angular';
+import { convertThemeName, registerTheme } from '@siemens/ix-echarts';
+import { EChartsOption } from 'echarts';
+import * as echarts from 'echarts/core';
+import { NgxEchartsModule } from 'ngx-echarts';
 
 import { facilities } from '../facility.mocks/const';
 import { IFacilityMock } from '../facility.mocks/facility.interface';
@@ -9,16 +14,55 @@ import { IFacilityMock } from '../facility.mocks/facility.interface';
 @Component({
 	selector: 'lib-detail',
 	standalone: true,
-	imports: [CommonModule, IxModule],
+	imports: [CommonModule, IxModule, NgxEchartsModule],
 	templateUrl: './detail.page.html',
 	styleUrl: './detail.page.scss',
 	encapsulation: ViewEncapsulation.None,
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class XdDetailPage {
+export class XdDetailPage implements OnInit {
 	facility: IFacilityMock = this.getFacility();
 
+	theme = convertThemeName(themeSwitcher.getCurrentTheme());
+	options: EChartsOption = {
+		xAxis: {
+			type: 'category',
+			data: [
+				'16:20',
+				'16:30',
+				'16:40',
+				'16:50',
+				'17:00',
+				'17:10',
+				'17:20',
+				'17:30',
+				'17:40',
+				'17:50',
+				'18:00',
+				'18:10',
+				'18:20',
+			],
+		},
+		yAxis: {
+			type: 'value',
+		},
+		series: [
+			{
+				data: [98, 88, 88, 102, 102, 88, 88, 88, 80, 76, 70, 72, 68],
+				type: 'line',
+			},
+		],
+	};
+
 	constructor(private route: ActivatedRoute) {}
+
+	ngOnInit() {
+		registerTheme(echarts);
+
+		themeSwitcher.themeChanged.on((theme: string) => {
+			this.theme = convertThemeName(theme);
+		});
+	}
 
 	getFacility(): IFacilityMock {
 		const facility = facilities.find(
