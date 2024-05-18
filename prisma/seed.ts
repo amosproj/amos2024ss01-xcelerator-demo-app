45ec8712bcb7def28549ff82a079c8ab5a5273e5
Jonas <jonas@mennicke.com>
2024-05-18 20:46:38 +0200

    fix: prisma schema

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,17 +1,10 @@
-import { PrismaClient, Prisma } from '@prisma/client';
+import { Prisma, PrismaClient } from '@prisma/client';
 import envData from './demo_data/PUMP-002_Environment_20240422-220000000_20240423-220000000.json';
 import pumpData from './demo_data/PUMP-002_PumpData_20240422-220000000_20240423-220000000.json';
 
 const prisma = new PrismaClient();
 
 async function main() {
-	const tSItemEnv = await prisma.timeSeriesItem.create({
-		data: {
-			entityId: 'Pump002',
-			propertySetName: 'Environment',
-		},
-	});
-
 	const tsItemPumpData = await prisma.timeSeriesItem.create({
 		data: {
 			entityId: 'Pump002',
@@ -19,11 +12,36 @@ async function main() {
 		},
 	});
 
+	const newPumpData = pumpData.map((data: any) => {
+		return {
+			time: data._time,
+
+			timeSeriesItementityId: tsItemPumpData.entityId,
+			timeSeriesItempropertySetName: tsItemPumpData.propertySetName,
+
+			data: {
+				motorCurrent: data.MotorCurrent,
+				pressureOut: data.PressureOut,
+				stuffingBoxTemperature: data.StuffingBoxTemperature,
+				pressureIn: data.PressureIn,
+				flow: data.Flow,
+			} as Prisma.JsonObject,
+		};
+	});
+
+	const tSItemEnv = await prisma.timeSeriesItem.create({
+		data: {
+			entityId: 'Pump002',
+			propertySetName: 'Environment',
+		},
+	});
+
 	const newEnvData = envData.map((data: any) => {
 		return {
 			time: data._time,
 
-			timeSeriesItemId: tSItemEnv.id,
+			timeSeriesItementityId: tSItemEnv.entityId,
+			timeSeriesItempropertySetName: tSItemEnv.propertySetName,
 
 			data: {
 				pressureQc: data.Pressure_qc,
@@ -36,28 +54,8 @@ async function main() {
 		};
 	});
 
-	const newPumpData = pumpData.map((data: any) => {
-		return {
-			time: data._time,
-
-			timeSeriesItemId: tsItemPumpData.id,
-
-			data: {
-				motorCurrent: data.MotorCurrent,
-				pressureOut: data.PressureOut,
-				stuffingBoxTemperature: data.StuffingBoxTemperature,
-				pressureIn: data.PressureIn,
-				flow: data.Flow,
-			} as Prisma.JsonObject,
-		};
-	});
-
 	await prisma.timeSeriesDataItem.createMany({
-		data: newPumpData,
-	});
-
-	await prisma.timeSeriesDataItem.createMany({
-		data: newEnvData,
+		data: [newPumpData, newEnvData].flat(),
 	});
 }
 
