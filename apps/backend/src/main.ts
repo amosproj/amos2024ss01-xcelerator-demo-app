19a2d515d715e6a3565e4047bd04b27cb77a734b
Jonas <jonas@mennicke.com>
2024-05-20 17:33:30 +0200

    feat: add transform for request & add get all timeseries
    Signed-off-by: Jonas <jonas@mennicke.com>

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -10,7 +10,11 @@ import { AppModule } from './app/app.module';
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 
-	app.useGlobalPipes(new ValidationPipe());
+	app.useGlobalPipes(
+		new ValidationPipe({
+			transform: true,
+		}),
+	);
 
 	const configService = app.get(ConfigService<BackendConfig>);
 
