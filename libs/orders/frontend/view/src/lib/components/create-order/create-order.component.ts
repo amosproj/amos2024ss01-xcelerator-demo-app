a80bca56fd8d0629d8d0be2ed28f1071eb473eb4
Patrick Schmidt <patrick.m.schmidt@fau.de>
2024-05-21 14:12:50 +0200

    feat: added form; TODO: fix ix-select validation and design
    Signed-off-by: Patrick Schmidt <patrick.m.schmidt@fau.de>

--- libs/orders/frontend/view/src/lib/components/create-order/create-order.component.ts
+++ libs/orders/frontend/view/src/lib/components/create-order/create-order.component.ts
@@ -1,26 +1,76 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormsModule, NgForm } from '@angular/forms';
 import { IxModule } from '@siemens/ix-angular';
+// eslint-disable-next-line @nx/enforce-module-boundaries
+import { facilities } from 'libs/facilities/frontend/view/src/lib/components/facility.mocks/const';
+// eslint-disable-next-line @nx/enforce-module-boundaries
+import { IFacilityMock } from 'libs/facilities/frontend/view/src/lib/components/facility.mocks/facility.interface';
 
 @Component({
 	selector: 'lib-create-order',
 	standalone: true,
-	imports: [CommonModule, IxModule, ReactiveFormsModule],
+	imports: [CommonModule, IxModule, FormsModule],
 	templateUrl: './create-order.component.html',
 	styleUrl: './create-order.component.scss',
 	encapsulation: ViewEncapsulation.None,
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CreateOrderComponent {
-	createOrderForm = new FormGroup({
-		selectFacility: new FormControl('', Validators.required),
-		assets: new FormControl('', Validators.required),
-		phone: new FormControl('', [Validators.required, Validators.maxLength(20)]),
-		email: new FormControl('', [Validators.required, Validators.email]),
-		text: new FormControl(''),
-	});
-	onSubmit() {
-		// submit order
+	facilities: IFacilityMock[] = facilities;
+
+	wasValidated = false;
+	value = '1';
+
+	createOrderForm = {
+		selectFacility: '',
+		selectAsset: '',
+		phone: '',
+		email: '',
+		text: '',
+	};
+
+	onSubmit(form: NgForm): void {
+		this.wasValidated = true;
+		if (form.form.valid) {
+			//Form is valid!!
+			//get all values by form.form.value
+		}
+	}
+
+	public setFacilityValue(value: string) {
+		this.createOrderForm.selectFacility = value;
+	}
+
+	public setAssetValue(value: string) {
+		this.createOrderForm.selectAsset = value;
+	}
+
+	public setPhoneValue(value: string) {
+		this.createOrderForm.phone = value;
+	}
+
+	public setEmailValue(value: string) {
+		this.createOrderForm.email = value;
+	}
+
+	public getFacilityValue() {
+		return this.createOrderForm.selectFacility;
+	}
+
+	public getAssetValue() {
+		return this.createOrderForm.selectAsset;
+	}
+
+	public getPhoneValue() {
+		return this.createOrderForm.phone;
+	}
+
+	public getEmailValue() {
+		return this.createOrderForm.email;
+	}
+
+	public getTextValue() {
+		return this.createOrderForm.text;
 	}
 }
