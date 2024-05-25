d861012d07238e168eb0c9090dd23bab3a283811
Ingo Sternberg <ingo.sternberg@fau.de>
2024-05-25 23:09:04 +0200

    fix: lint, tests, slightly bigger charts
    Signed-off-by: Ingo Sternberg <ingo.sternberg@fau.de>
    #79

--- apps/frontend/src/app/app.config.ts
+++ apps/frontend/src/app/app.config.ts
@@ -3,11 +3,11 @@ import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { IxModule } from '@siemens/ix-angular';
 import { APP_CONFIG } from 'common-frontend-models';
+import { provideEcharts } from 'ngx-echarts';
 
 import { environment } from '../environments/environment';
 import { BackendUrlInterceptor } from '../models/interceptors/backend-url.interceptor';
 import { APP_ROUTES } from './app.routes';
-import { provideEcharts } from 'ngx-echarts';
 
 export const appConfig: ApplicationConfig = {
 	providers: [
--- libs/facilities/frontend/view/src/lib/components/detail/chart/chart.component.spec.ts
+++ libs/facilities/frontend/view/src/lib/components/detail/chart/chart.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideEcharts } from 'ngx-echarts';
 
 import { ChartComponent } from './chart.component';
 
@@ -6,9 +7,18 @@ describe('ChartComponent', () => {
 	let component: ChartComponent;
 	let fixture: ComponentFixture<ChartComponent>;
 
+    window.ResizeObserver =
+        window.ResizeObserver ||
+        jest.fn().mockImplementation(() => ({
+            disconnect: jest.fn(),
+            observe: jest.fn(),
+            unobserve: jest.fn(),
+        }));
+
 	beforeEach(async () => {
 		await TestBed.configureTestingModule({
 			imports: [ ChartComponent ],
+            providers: [ provideEcharts() ],
 		}).compileComponents();
 
 		fixture = TestBed.createComponent(ChartComponent);
