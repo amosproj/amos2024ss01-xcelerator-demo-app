8c04d49e98dee06237ee4a3e12e387017b4197f1
Sabo <saleb2k@gmail.com>
2024-05-26 01:30:04 +0200

    refactor(backend): renamed case-management library
    from case-managament to case-management
    
    #68

--- libs/case-management/jest.config.ts
+++ libs/case-management/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
-	displayName: 'case-managament',
-	preset: '../../../../jest.preset.js',
+	displayName: 'case-management',
+	preset: '../../jest.preset.js',
 	testEnvironment: 'node',
 	transform: {
 		'^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
 	},
 	moduleFileExtensions: ['ts', 'js', 'html'],
-	coverageDirectory: '../../../../coverage/libs/cases/backend/case-managament',
+	coverageDirectory: '../../coverage/libs/case-management',
 };
--- libs/cases/backend/case-managament/README.md
+++
@@ -1,7 +0,0 @@
-# case-managament
-
-This library was generated with [Nx](https://nx.dev).
-
-## Running unit tests
-
-Run `nx test case-managament` to execute the unit tests via [Jest](https://jestjs.io).
