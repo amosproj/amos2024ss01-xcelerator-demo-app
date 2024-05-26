ee539fc2b9397c878c83d81071e2a54b18f43fb4
Sabo <saleb2k@gmail.com>
2024-05-26 10:50:01 +0200

    refactor(backend): moved case library
    new path: /libs/cases/backend/case-management/
    
    #68

--- libs/cases/backend/case-management/jest.config.ts
+++ libs/cases/backend/case-management/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
 	displayName: 'case-management',
-	preset: '../../jest.preset.js',
+	preset: '../../../../jest.preset.js',
 	testEnvironment: 'node',
 	transform: {
 		'^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
 	},
 	moduleFileExtensions: ['ts', 'js', 'html'],
-	coverageDirectory: '../../coverage/libs/case-management',
+	coverageDirectory: '../../../../coverage/libs/cases/backend/case-management',
 };
