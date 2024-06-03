0e2ad82587f6d3a5288dbc4529dec59b6e7bebcf
Maximilian Krug <krugm03@gmail.com>
2024-06-03 18:38:50 +0200

    refactor: 108 make the header happy
    Signed-off-by: Maximilian Krug <krugm03@gmail.com>
    #108

--- apps/frontend/src/app/components/header/header.component.ts
+++ apps/frontend/src/app/components/header/header.component.ts
@@ -26,7 +26,7 @@ export interface IBreadcrumbData {
 @Component({
 	selector: 'app-header',
 	standalone: true,
-	imports: [ CommonModule, IxModule, RouterLink, RouterOutlet ],
+	imports: [CommonModule, IxModule, RouterLink, RouterOutlet],
 	templateUrl: './header.component.html',
 	styleUrl: './header.component.scss',
 	encapsulation: ViewEncapsulation.None,
@@ -40,9 +40,61 @@ export class HeaderComponent {
 		this._router.events.pipe(filter((e) => e instanceof NavigationEnd)),
 		{ initialValue: null },
 	);
+
 	readonly breadcrumbs = computed(() => {
 		this.routerEvents();
 		return HeaderComponent.buildBreadcrumbRecursively(this._activatedRoute.root);
+	});
+
+	readonly headerData = computed(() => {
+		const breadcrumbs = this.breadcrumbs();
+		let tempHeader = '';
+
+		const headerMap = new Map<string, string>([
+			['Issues', 'Open Issues'],
+			['Facilities', 'Facilities Dashboard'],
+			['Home', 'Homepage'],
+			['Cases', 'Cases'],
+			['Open', 'Open Cases'],
+		]);
+
+		const titleMap = new Map<string, string>([
+			['Issues', 'List of all Facilities with Issues'],
+			['Facilities', 'List of all Facilities'],
+			['Home', ''],
+			['Cases', ''],
+			['Open', 'List of all Open Cases'],
+		]);
+
+		if (breadcrumbs.length > 0) {
+			tempHeader = breadcrumbs[breadcrumbs.length - 1].label;
+		} else {
+			return { title: '', subtitle: '' };
+		}
+
+		// Check for special case where label is "Details"
+		if (tempHeader === 'Details') {
+			const previousBreadcrumb =
+				breadcrumbs.length > 1 ? breadcrumbs[breadcrumbs.length - 2].label : '';
+
+			if (previousBreadcrumb === 'Facilities') {
+				return {
+					title: 'Facility Details Page',
+					subtitle: 'Detailed information about a specific facility',
+				};
+			} else if (previousBreadcrumb === 'Cases') {
+				return {
+					title: 'Case Details Page',
+					subtitle: 'Detailed information about a specific case',
+				};
+			}
+		}
+
+		if (headerMap.has(tempHeader)) {
+			return { title: headerMap.get(tempHeader), subtitle: titleMap.get(tempHeader) || '' };
+		} else {
+			return { title: tempHeader, subtitle: '' };
+		}
 	});
 
 	/**
@@ -65,5 +117,9 @@ export class HeaderComponent {
 		}
 
 		return this.buildBreadcrumbRecursively(route.firstChild, breadcrumbs);
+	}
+
+	goBack() {
+		this._router.navigate([this._router.url.split('/').slice(0, -1).join('/')]);
 	}
 }
