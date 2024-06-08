eb45faab3550ac367bad899316255dad1be31aab
Jonas Heisterberg <jonas@mennicke.com>
2024-06-08 21:05:40 +0200

    refactor: update seeder :seedling:
    Signed-off-by: Jonas Heisterberg <jonas@mennicke.com>
    Signed-off-by: Jonas <jonas@mennicke.com>

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -5,19 +5,6 @@ import pumpData from './demo_data/PUMP-002_PumpData_20240422-220000000_20240423-
 
 const prisma = new PrismaClient();
 
-const seedFacilities = [
-	'Pump001',
-	'Pump002',
-	'Pump003',
-	'Pump004',
-	'Pump005',
-	'Pump006',
-	'Pump007',
-	'Pump008',
-	'Pump009',
-	'Pump010',
-];
-
 async function main() {
 	const asset = await prisma.asset.create({
 		data: {
@@ -66,53 +53,16 @@ async function main() {
 			timeSeriesAssetId: asset.id,
 			timeSeriesItemPropertySetName: tSItemEnv.propertySetName,
 
-		const tSItemEnv = await prisma.timeSeriesItem.create({
 			data: {
-				entityId: facility,
-				propertySetName: 'Environment',
-			},
-		});
-
-		const newPumpData = pumpData.map((data: any) => {
-			return {
-				time: data._time as Date,
-
-				timeSeriesItementityId: tsItemPumpData.entityId,
-				timeSeriesItempropertySetName: tsItemPumpData.propertySetName,
-
-				data: {
-					motorCurrent: data.MotorCurrent,
-					pressureOut: data.PressureOut,
-					stuffingBoxTemperature: data.StuffingBoxTemperature,
-					pressureIn: data.PressureIn,
-					flow: data.Flow,
-				} as Prisma.JsonObject,
-			};
-		});
-
-		const newEnvData = envData.map((data: any) => {
-			return {
-				time: data._time as Date,
-
-				timeSeriesItementityId: tSItemEnv.entityId,
-				timeSeriesItempropertySetName: tSItemEnv.propertySetName,
-
-				data: {
-					pressureQc: data.Pressure_qc,
-					temperature: data.Temperature,
-					temperatureQc: data.Temperature_qc,
-					humidityQc: data.Humidity_qc,
-					humidity: data.Humidity,
-					pressure: data.Pressure,
-				} as Prisma.JsonObject,
-			};
-		});
-
-		// Seed database with timeseries data
-		await prisma.timeSeriesDataItem.createMany({
-			data: [newPumpData, newEnvData].flat(),
-		});
-	}
+				pressureQc: data.Pressure_qc,
+				temperature: data.Temperature,
+				temperatureQc: data.Temperature_qc,
+				humidityQc: data.Humidity_qc,
+				humidity: data.Humidity,
+				pressure: data.Pressure,
+			} as Prisma.JsonObject,
+		};
+	});
 
 	// create new case data from JSON file
 	const newCaseData = caseData.map((data: any) => {
@@ -128,6 +78,11 @@ async function main() {
 			createdBy: data.createdBy,
 			eTag: data.eTag,
 		};
+	});
+
+	// Seed database with timeseries data
+	await prisma.timeSeriesDataItem.createMany({
+		data: [newPumpData, newEnvData].flat(),
 	});
 
 	// Seed database with case data
