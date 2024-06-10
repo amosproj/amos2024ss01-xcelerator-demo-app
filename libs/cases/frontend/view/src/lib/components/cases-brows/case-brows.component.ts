4357dcb1ce29e94f0a224dd3db324f336627a21d
Alexander Lorenz <alex.lorenz.1@gmx.de>
2024-06-10 19:25:07 +0200

    feat: 144: Improved code-quality
    Signed-off-by: Alexander Lorenz <alex.lorenz.1@gmx.de>

--- libs/cases/frontend/view/src/lib/components/cases-brows/case-brows.component.ts
+++ libs/cases/frontend/view/src/lib/components/cases-brows/case-brows.component.ts
@@ -1,8 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, ViewEncapsulation, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { IxModule } from '@siemens/ix-angular';
 
+import { ICaseMock } from '../case.mocks/case.interface';
 import { cases } from '../case.mocks/const';
 
 @Component({
@@ -15,43 +16,35 @@ import { cases } from '../case.mocks/const';
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CaseBrowsComponent {
-	protected readonly _cases = cases;
-	sortedCases: any[];
+    protected readonly _cases = cases;
+    sortedCases: ICaseMock[];
 
-	ngOnInit(): void {
-		const statusOrder = [
-			'OPEN',
-			'INPROGRESS',
-			'OVERDUE',
-			'ONHOLD',
-			'DONE',
-			'CANCELLED',
-			'ARCHIVED',
-		];
-		const priorityOrder = ['EMERGENCY', 'HIGH', 'MEDIUM', 'LOW'];
+    ngOnInit(): void {
+        const statusOrder = ['OPEN', 'INPROGRESS', 'OVERDUE', 'ONHOLD', 'DONE', 'CANCELLED', 'ARCHIVED'];
+        const priorityOrder = ['EMERGENCY', 'HIGH', 'MEDIUM', 'LOW'];
 
-		this.sortedCases = [...this._cases].sort((a, b) => {
-			const statusAIndex = statusOrder.indexOf(a.status);
-			const statusBIndex = statusOrder.indexOf(b.status);
+        this.sortedCases = [...this._cases].sort((a, b) => {
+            const statusAIndex = statusOrder.indexOf(a.status);
+            const statusBIndex = statusOrder.indexOf(b.status);
 
-			if (statusAIndex === statusBIndex) {
-				const priorityAIndex = priorityOrder.indexOf(a.priority.toUpperCase());
-				const priorityBIndex = priorityOrder.indexOf(b.priority.toUpperCase());
+            if (statusAIndex === statusBIndex) {
+                const priorityAIndex = priorityOrder.indexOf(a.priority.toUpperCase());
+                const priorityBIndex = priorityOrder.indexOf(b.priority.toUpperCase());
 
-				if (priorityAIndex === priorityBIndex) {
-					return a.handle.localeCompare(b.handle);
-				} else if (priorityAIndex === -1) {
-					return 1;
-				} else if (priorityBIndex === -1) {
-					return -1;
-				}
-				return priorityAIndex - priorityBIndex;
-			} else if (statusAIndex === -1) {
-				return 1;
-			} else if (statusBIndex === -1) {
-				return -1;
-			}
-			return statusAIndex - statusBIndex;
-		});
-	}
+                if (priorityAIndex === priorityBIndex) {
+                    return a.handle.localeCompare(b.handle);
+                } else if (priorityAIndex === -1) {
+                    return 1;
+                } else if (priorityBIndex === -1) {
+                    return -1;
+                }
+                return priorityAIndex - priorityBIndex;
+            } else if (statusAIndex === -1) {
+                return 1;
+            } else if (statusBIndex === -1) {
+                return -1;
+            }
+            return statusAIndex - statusBIndex;
+        });
+    }
 }
--- libs/cases/frontend/view/src/lib/components/open-cases/open-cases.component.ts
+++ libs/cases/frontend/view/src/lib/components/open-cases/open-cases.component.ts
@@ -1,8 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, ViewEncapsulation, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit,ViewEncapsulation } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { IxModule } from '@siemens/ix-angular';
 
+import { ICaseMock } from '../case.mocks/case.interface';
 import { cases } from '../case.mocks/const';
 
 @Component({
@@ -15,25 +16,23 @@ import { cases } from '../case.mocks/const';
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class OpenCasesComponent implements OnInit {
-	protected readonly _cases = cases;
-	filteredCases: any[];
+    protected readonly _cases = cases;
+    filteredCases: ICaseMock[];
 
-	ngOnInit(): void {
-		this.filteredCases = this._cases
-			.filter((_case) => _case.status === 'OPEN')
-			.sort((a, b) => {
-				const priorityOrder = ['EMERGENCY', 'HIGH', 'MEDIUM', 'LOW'];
-				const priorityAIndex = priorityOrder.indexOf(a.priority.toUpperCase());
-				const priorityBIndex = priorityOrder.indexOf(b.priority.toUpperCase());
+    ngOnInit(): void {
+        this.filteredCases = this._cases.filter(_case => _case.status === 'OPEN').sort((a, b) => {
+            const priorityOrder = ['EMERGENCY', 'HIGH', 'MEDIUM', 'LOW'];
+            const priorityAIndex = priorityOrder.indexOf(a.priority.toUpperCase());
+            const priorityBIndex = priorityOrder.indexOf(b.priority.toUpperCase());
 
-				if (priorityAIndex === priorityBIndex) {
-					return a.handle.localeCompare(b.handle);
-				} else if (priorityAIndex === -1) {
-					return 1;
-				} else if (priorityBIndex === -1) {
-					return -1;
-				}
-				return priorityAIndex - priorityBIndex;
-			});
-	}
+            if (priorityAIndex === priorityBIndex) {
+                return a.handle.localeCompare(b.handle);
+            } else if (priorityAIndex === -1) {
+                return 1;
+            } else if (priorityBIndex === -1) {
+                return -1;
+            }
+            return priorityAIndex - priorityBIndex;
+        });
+    }
 }
