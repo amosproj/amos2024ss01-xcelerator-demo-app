2a9f3dfaefaf7774336ba8e6372e756b8c2cd7a9
Jonas Heisterberg <jonas@mennicke.com>
2024-06-10 16:34:44 +0200

    refactor: fix facilitie imports
    Signed-off-by: Jonas Heisterberg <jonas@mennicke.com>
    Signed-off-by: Jonas <jonas@mennicke.com>

--- libs/facilities/backend/facilities/src/lib/facilities.module.ts
+++ libs/facilities/backend/facilities/src/lib/facilities.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { XdInsightHubModule } from 'common-backend-insight-hub';
+import { XdPrismaModule } from 'common-backend-prisma';
 
 import { XdFacilitiesController } from './controller/facilities.controller';
 import { XdFacilitesService } from './service/faclities.service';
-import { XdPrismaModule } from 'common-backend-prisma';
 
 @Module({
 	controllers: [XdFacilitiesController],
