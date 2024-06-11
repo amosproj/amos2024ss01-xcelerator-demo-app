20f6cbc32b0642933ef1e2d27bf07759fffb1715
Patrick Schmidt <patrick.m.schmidt@fau.de>
2024-06-11 20:28:19 +0200

    refactor: removed the logo and added some small changes
    Signed-off-by: Patrick Schmidt <patrick.m.schmidt@fau.de>
    #111

--- apps/frontend/src/app/components/header/legal-information/legal-information.component.ts
+++ apps/frontend/src/app/components/header/legal-information/legal-information.component.ts
@@ -1,10 +1,7 @@
 import { CommonModule } from '@angular/common';
 import {
-	AfterViewInit,
 	ChangeDetectionStrategy,
 	Component,
-	ElementRef,
-	ViewChild,
 	ViewEncapsulation,
 } from '@angular/core';
 import { IxModule } from '@siemens/ix-angular';
@@ -12,18 +9,10 @@ import { IxModule } from '@siemens/ix-angular';
 @Component({
 	selector: 'app-legal-information',
 	standalone: true,
-	imports: [CommonModule, IxModule],
+	imports: [ CommonModule, IxModule ],
 	templateUrl: './legal-information.component.html',
 	styleUrl: './legal-information.component.scss',
 	encapsulation: ViewEncapsulation.None,
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LegalInformationComponent implements AfterViewInit {
-	@ViewChild('menu', { read: ElementRef })
-	menuRef!: ElementRef<HTMLIxMenuElement>;
-
-	ngAfterViewInit() {
-		const { nativeElement } = this.menuRef;
-		nativeElement.toggleAbout(true);
-	}
-}
+export class LegalInformationComponent {}
