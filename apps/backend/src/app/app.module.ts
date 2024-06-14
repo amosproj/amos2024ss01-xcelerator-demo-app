085ac2e0201daf80b8b1180acbcc883c9061e1a1
Jonas <jonas@mennicke.com>
2024-06-14 17:43:55 +0200

    fix: lint
    Signed-off-by: Jonas <jonas@mennicke.com>

--- apps/backend/src/app/app.module.ts
+++ apps/backend/src/app/app.module.ts
@@ -1,7 +1,7 @@
-import { XdInsightHubModule } from 'common-backend-insight-hub';
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { XdCaseManagementModule } from 'cases-backend-case-management';
+import { XdInsightHubModule } from 'common-backend-insight-hub';
 import { BackendConfig } from 'common-backend-models';
 import { XdFacilitiesBackendFacilitiesModule } from 'facilities-backend-facilities';
 import { XdTimeseriesModule } from 'facilities-backend-timeseries';
