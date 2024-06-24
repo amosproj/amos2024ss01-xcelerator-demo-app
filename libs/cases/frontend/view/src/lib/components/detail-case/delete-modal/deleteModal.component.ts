8776e465de225b2fad094f6a63dc3fb567b68029
Maximilian Krug <krugm03@gmail.com>
2024-06-24 19:07:39 +0200

    refactor: fixed the linting
    Signed-off-by: Maximilian Krug <krugm03@gmail.com>
    #161

--- libs/cases/frontend/view/src/lib/components/detail-case/delete-modal/deleteModal.component.ts
+++ libs/cases/frontend/view/src/lib/components/detail-case/delete-modal/deleteModal.component.ts
@@ -1,11 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
-import { IxActiveModal, IxModule } from '@siemens/ix-angular';
 import { RouterLink } from '@angular/router';
+import { IxActiveModal, IxModule } from '@siemens/ix-angular';
 @Component({
 	selector: 'lib-lock-modal',
 	standalone: true,
-    imports: [CommonModule, IxModule, RouterLink],
+    imports: [ CommonModule, IxModule, RouterLink ],
 	templateUrl: './deleteModal.component.html',
 	styleUrl: './deleteModal.component.scss',
 	encapsulation: ViewEncapsulation.None,
